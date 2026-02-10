ac1ae1e69969367cf4f6d6ca9ce31ef8c1f4f2df
Choong Min Um <choongmin.um@unh.edu>
2026-02-09 20:08:35 -0800

    feat: use the system clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Sets the clipboard to use the system clipboard
+vim.g.clipboard = "osc52"
+
 -- Converts tab characters into spaces when pressing the Tab key
 vim.opt.expandtab = true
 
