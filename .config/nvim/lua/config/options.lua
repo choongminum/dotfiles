243448b73c03547254839ba3e76f595ddf3b78dc
Choong Min Um <choongmin.um@unh.edu>
2026-02-09 19:08:27 -0800

    feat: initial commit

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,21 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Converts tab characters into spaces when pressing the Tab key
+vim.opt.expandtab = true
+
+-- Controls whether lines soft-wrap at words, not characters
+vim.opt.linebreak = true
+
+-- Defines the number of spaces to use for each level of indentation
+vim.opt.shiftwidth = 4
+
+-- Defines the number of spaces the cursor moves when pressing the Tab key
+vim.opt.softtabstop = 4
+
+-- Defines the number of screen columns that a tab character (\t) will occupy when displayed
+vim.opt.tabstop = 4
+
+-- Controls whether lines soft-wrap to fit within the editor window or extend horizontally
+vim.opt.wrap = true
