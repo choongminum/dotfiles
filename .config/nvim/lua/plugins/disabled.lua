a74eba4776be3dede4dad08984683056fdcf7dd8
Choong Min Um <choongmin.um@unh.edu>
2026-06-08 00:15:09 -0700

    feat: edit files per LazyVim tutorial standards

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        enabled = false,
+    },
+    {
+        "folke/snacks.nvim",
+        keys = {
+            {
+                "<leader>e",
+                false,
+            },
+            {
+                "<leader>E",
+                false,
+            },
+        },
+        opts = {
+            explorer = {
+                enabled = false,
+            },
+        },
+    },
+}
